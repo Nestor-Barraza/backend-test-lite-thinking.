a045fc9b89118e8ec420c6b9474973c7e1a2b4e6
nestor-barraza <nestorbarrazaotalvarez@hotmail.com>
2023-03-06 03:47:37 -0500

    update server to HTTPS Protocol

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,19 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 var _defaultTemplate = _interopRequireDefault(require("./utils/defaultTemplate"));
 var _server = _interopRequireDefault(require("./server"));
+var _express = _interopRequireDefault(require("express"));
+var _https = _interopRequireDefault(require("https"));
+var _fs = _interopRequireDefault(require("fs"));
 require("colors");
 // Get port
 const PORT = _server.default.get("port");
+const options = {
+  key: _fs.default.readFileSync("key.pem"),
+  cert: _fs.default.readFileSync("cert.pem")
+};
 
 //listen server
-_server.default.listen(PORT, () => {
+_https.default.createServer(options, _server.default).listen(PORT, () => {
   try {
     console.log("Listen on port:".blue, `${PORT}`.rainbow.underline);
     _server.default.use("/", (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import DefaultTemplate from "utils/defaultTemplate";
 import app from "./server";
+import express from "express";
+import https from "https";
+import fs from "fs";
 import "colors";
 
 // Get port
 const PORT = app.get("port");
 
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+};
+
 //listen server
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   try {
     console.log("Listen on port:".blue, `${PORT}`.rainbow.underline);
     app.use("/", (req, res) => {
